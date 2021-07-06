acb94596a9608d244167bca48899794d9f97a427
ms3nd3r <casm1200160@gn.iwasaki.ac.jp>
2021-07-06 11:25:34 +0900

    システム完成、index.htmlのページスタイル調整

--- Emotions_meter/result.php
+++ Emotions_meter/result.php
@@ -5,38 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="set.css">
 </head>
 <body>
     <?php
-    $souhyou=$_POST["souhyou"];
-    $hap = $_POST["happy"];
-    $fun = $_POST["fun"];
-    $sad = $_POST["sad"];
-    $ang = $_POST["anger"];
-    $pts = 0;
-    $goodComment = array("いい一日だったね！","今日は最高だ！","よく頑張ったね！");
-    $badComment = array("そういう日もある","明日はきっと良いことあるよ","お疲れ様です。")
-    ?>
-    <p>今日は・・・</p><p id="msg5"><?php 
-        $pts = $hap+$fun-$sad-$ang;
+    $souhyou=htmlspecialchars($_POST["souhyou"],ENT_QUOTES,"UTF-8");
+    $hap = htmlspecialchars($_POST["happy"],ENT_QUOTES,"UTF-8");
+    $fun = htmlspecialchars($_POST["fun"],ENT_QUOTES,"UTF-8");
+    $sad = htmlspecialchars($_POST["sad"],ENT_QUOTES,"UTF-8");
+    $ang = htmlspecialchars($_POST["anger"],ENT_QUOTES,"UTF-8");
+    $goodComment = array("いい一日だったね！","最高だ！","よく頑張ったね！");
+    $badComment = array("そういう日もある。","明日はきっと良いことあるよ。","お疲れ様です。");
+    $pts = $hap+$fun-$sad-$ang;
         $rnd = mt_rand(0,2);
-        if($pts>= 100){
-            print $goodComment[$rnd];
-        }elseif($pts<50){
-            print "イイ感じですね";
+        if($pts>75){
+            $result= '$pts."点！！".$goodComment[$rnd]'; //この文字列を変数化
+            $day='goodDay';
+        }elseif($pts>=25){
+            $result=  '$pts."。イイ感じですね"';
+            $day='sosoDay';
         }else{
-            print $badComment[$rnd];
+            $result= '$pts."点です。".$badComment[$rnd]';
+            $day='badDay';
         }
+    print '<div id=".$day.">';
+    print  '<p>今日は・・・</p><p id="msg5">';
+    print $result;//上の変数    
+
     ?> <!--いい一日だったね！とかよく頑張った！とか点数に応じてポジティブな一言-->
-    
-    今日の感情をツイートする→<a href="https://twitter.com/share" class="twitter-share-button" 
-    data-url="ここに授業用のドメインを入れる" data-text="<?php print $souhyou ?>：こんな一日を過ごしていました。" data-via="【ユーザ名】" 
-    data-size="large" data-related="ms3nd3r" data-count="【カウント表示の種類】" 
-    data-hashtags="エモメーター">tweet</a>  <!--可能なら総評のテキストを持ってきてツイート出来るようにする-->
-    <script>//twitter用
-    !function(d,s,id)
-    {var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-    </script>
+
+    今日の感情をツイートする→<a href="https://twitter.com/share?url=http://r03isc2t751.sub.jp/Emotion_meter/&hashtags=エモメーター&text=<?php print $souhyou.":今日はこんな過ごし方で".$pts."点な一日でした。";?>"
+    rel="nofollow"
+    target="_blank">tweet</a>
+    </div>
 
 </body>
 </html>
